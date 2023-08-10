5b83519b86a632bc9f702305260b82f0e7dd7b2c
Jadefaze <mabnoz@gmail.com>
2023-08-11 02:31:41 +0300

    fixed betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,7 @@ int main(void)
 	long int l;
 	long long int li;
 	float f;
+
 	printf("Size of a char: %i byte(s) \n", sizeof(c));
 	printf("Size of an int: %i byte(s) \n", sizeof(i));
 	printf("Size of a long int: %i byte(s) \n", sizeof(l));
