f641e487740dd34a8ae5c3c62e401157142c4eb2
Jadefaze <mabnoz@gmail.com>
2023-08-12 00:31:22 +0300

    fixed continuation line with \

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 
 	if (last_digit > 5)
 	{
-		printf("Last digit of %i is %i and is greater
+		printf("Last digit of %i is %i and is greater\
 				than 5\n", n, last_digit);
 	}
 	else if (last_digit == 0)
@@ -29,7 +29,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %i is %i and is less
+		printf("Last digit of %i is %i and is less\
 				than 6 and not 0\n", n, last_digit);
 	}
 	return (0);
