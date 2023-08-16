bb3f7e1dfefbe2df795f09d0f3f4e871447a431e
Jadefaze <mabnoz@gmail.com>
2023-08-17 00:14:54 +0300

    prototyping

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Description: to print alphabet
+ * Return: 0 Always.
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
