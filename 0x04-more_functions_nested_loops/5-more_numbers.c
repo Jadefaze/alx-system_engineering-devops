8eeb2abfea388f24994ff84e43c83c34360c4776
Jadefaze <mabnoz@gmail.com>
2023-08-26 13:21:44 +0300

    loops and conditionals

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,22 +10,18 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	j = 1;
-	while (j <= 10)
+	for (j = 1; j <= 10; j++)
 	{
-		for (i = 0; i <= 9; i++)
+		i = 0;
+		while (i <= 14)
 		{
-			_putchar(i + '0');
+			if (i > 9)
+			{
+				_putchar(49);
+			}
+			_putchar(i % 10 + '0');
+			i++;
 		}
-		for (i = 10; i <=14; i++)
-		{	
-			_putchar(i%10 + '0');
-		}
-	_putchar('\n');
-	j++;	
-
+		_putchar('\n');
 	}
-
-
-
 }
