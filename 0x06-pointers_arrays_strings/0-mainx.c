2542ff64c98b76c445457e270426dd8bc241157d
Jadefaze <mabnoz@gmail.com>
2023-09-07 18:53:57 +0300

    concat raw

---
+++ 0x06-pointers_arrays_strings/0-mainx.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	s1[98] = "hello";
+	s2[] = "World";
+	char *ptr;
+
+	ptr = _strcat(s2, s1);
+
+	printf("
+
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strncpy - to copy string
+ * @dest: the new string
+ * @src: the first string
+ * @n: atmost bytes from src
+ * Return: pointer to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (dest);
+}
