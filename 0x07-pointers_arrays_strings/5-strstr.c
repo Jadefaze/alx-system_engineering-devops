900cf14c7245a750a11be0b4fabd394c4f8f27e9
Jadefaze <mabnoz@gmail.com>
2023-09-11 02:50:17 +0300

    search substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,4 @@
-#include 'main.h'
+#include "main.h"
 
 /**
  * _strstr - locate substring
@@ -11,5 +11,28 @@
 
 char *_strstr(char *haystack, char *needle)
 {
+	int i;
+	int j;
+	int size; /*size of needle string*/
 
+	size = 0;
+	i = 0;
+	while (*(needle + size) != '\0')
+	{
+		size++;
+	}
+	for (; *(haystack + i) != '\0'; i++)
+	{
+		j = 0;
+		while (haystack[i] == needle[j] && haystack[i] != '\0')
+		{
+			if (j + 1 == size)
+			{
+				return (haystack + i - j);
+			}
+			j++;
+			i++;
+		}
+	}
+	return (0);
 }
