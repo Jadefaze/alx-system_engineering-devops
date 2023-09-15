5a5aa29c3803abba76224450a822c36ee4b95511
Jadefaze <mabnoz@gmail.com>
2023-09-15 23:43:24 +0300

    fixed NULL pointer case and betty style of header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,6 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
 #include <stddef.h>
 
 int _putchar(char c);
@@ -9,4 +12,6 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+
+#endif /*FUNCTION_POINTERS_H*/
 
