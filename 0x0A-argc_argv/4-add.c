fba6de210e1476425428ddf0c6083b3b2dcd4418
Jadefaze <mabnoz@gmail.com>
2023-10-03 02:18:33 +0300

    5th task on argc argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - add args passed
+ * @argc: number of args
+ * @argv: vector of cmds passed
+ * Return: 0 else 1 for error
+ */
+
+int main(int argc, char *argv[])
+{
+	int result, i;
+
+	i = 1;
+	result = 0;
+	while (i < argc)
+	{
+		if (!(atoi(argv[i])))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		result += atoi(argv[i]);
+		i++;
+	}
+	printf("%d\n", result);
+
+	return (0);
+}
