b81f98e4e78a725f0488fabeac9827cfaca6fde5
Jadefaze <mabnoz@gmail.com>
2023-10-07 05:20:39 -0700

    malloc 3

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,14 +11,15 @@ char *_strdup(char *str)
 	char *str_dup;
 	int i, str_len;
 
+	str_len = 0;
+
 	if (str == NULL)
 		return (NULL);
 
 	/* size of str */
-	while (str[i] != '\0')
-		i++;
+	while (str[str_len] != '\0')
+		str_len++;
 
-	str_len = i;
 	str_dup = malloc(sizeof(char) * (str_len + 1));
 
 	if (!str_dup)
